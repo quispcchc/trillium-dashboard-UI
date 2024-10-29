9befb26995b146df59d937bbed6fe60a6dfa5116
quispcchc <quisp@carlingtonchc.org>
2024-10-29 13:16:12 -0400

    implement new design on dashboard footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+
+  @Output() changeTab = new EventEmitter<string>();
+  @Output() logoutUser = new EventEmitter<void>();
+
+  userRole: string | null | undefined;
+
+  ngOnInit(): void {
+    this.userRole = localStorage.getItem('role');
+  }
+
+  switchTab(name: string): void {
+    this.changeTab.emit(name);
+    window.scroll({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  logout():void {
+    this.logoutUser.emit();
+  }
+
 }
