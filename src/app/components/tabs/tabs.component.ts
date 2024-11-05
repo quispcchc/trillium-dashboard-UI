7959b6b5d42a964dee5b48c6d4f4f5c59a39ef2d
quispcchc <quisp@carlingtonchc.org>
2024-11-04 21:12:46 -0500

    fix merge conflict

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 
 @Component({
@@ -6,6 +6,16 @@ import { Component, Input } from '@angular/core';
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.css']
 })
-export class TabsComponent {
+
+export class TabsComponent implements OnInit {
   @Input() selectedTab!: string;
+  userRole: string | null | undefined;
+
+  ngOnInit(): void {
+    this.userRole = localStorage.getItem('role');
+  }
+  
+  selectTab(tab: string): void {
+    this.selectedTab = tab;
+  }
 }
