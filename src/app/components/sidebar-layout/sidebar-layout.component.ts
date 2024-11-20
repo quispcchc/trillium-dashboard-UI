e9f8da763a1189b9eb2283876678154f36512dc2
quispcchc <quisp@carlingtonchc.org>
2024-11-20 12:04:08 -0500

    remove navigation list filtering implementation

--- src/app/components/sidebar-layout/sidebar-layout.component.ts
+++ src/app/components/sidebar-layout/sidebar-layout.component.ts
@@ -1,28 +1,14 @@
-import { Component, Input,OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar-layout',
   templateUrl: './sidebar-layout.component.html',
   styleUrl: './sidebar-layout.component.css'
 })
-export class SidebarLayoutComponent implements OnInit{
+export class SidebarLayoutComponent {
   @Input() navList!: { name: string, label: string, action: () => void }[] | undefined;
   @Input() activeNav!: string;
   @ViewChild('sidebar') sidebar!: ElementRef;
-  accessibleForms: string  | null | undefined;
-  filteredNavList: { name: string, label: string, action: () => void }[] = [];
-
-  ngOnInit(): void {
-    this.accessibleForms = localStorage.getItem('accessible_forms');
-    if (this.navList) {
-      this.filteredNavList = this.navList.filter(btn => this.isFormAccessible(btn.label));
-    }
-  }
-
-  isFormAccessible(form: string): boolean {
-    const accessibleFormsArray = this.accessibleForms ? this.accessibleForms.split(',').map(form => form.trim()) : [];
-    return accessibleFormsArray.includes(form.trim());
-  }  
 
   scrollUp() {
     if (window.matchMedia('(max-width: 767px)').matches){
