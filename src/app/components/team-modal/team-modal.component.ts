93b4bebedd8924c0c451402f20264af5065ecca3
quispcchc <quisp@carlingtonchc.org>
2024-12-10 12:22:40 -0500

    update team modal to include team info

--- src/app/components/team-modal/team-modal.component.ts
+++ src/app/components/team-modal/team-modal.component.ts
@@ -1,16 +1,107 @@
-import { Component, Input, Output, EventEmitter,  } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-team-modal',
   templateUrl: './team-modal.component.html',
   styleUrl: './team-modal.component.css'
 })
-export class TeamModalComponent {
+export class TeamModalComponent implements OnInit {
 
   @Input() modalOpen!:boolean;
   @Output() closeModal = new EventEmitter<void>();
 
   modalClosed: boolean = false;
+  teamList: {name: string, title: string, img: string, imgAvailable: boolean, linkedIn: string }[] = []
+
+  ngOnInit(): void {
+    this.teamList = [
+      {
+        name: 'Ahmet Kapici',
+        title: 'Manager of quality improvement and special projects',
+        img: 'team/Ahmet.png',
+        imgAvailable: true,
+        linkedIn: 'https://www.linkedin.com/in/ahmetkapici'
+      },
+      {
+        name: 'Kaoutar Fairouzane',
+        title: 'Data Analyst',
+        img: '',
+        imgAvailable: false,
+        linkedIn: 'http://www.linkedin.com/in/kaoutar-fairouzane-6a149a37'
+      },
+      {
+        name: 'Mariam El Haiba',
+        title: 'Project Manager',
+        img: 'team/Mariam_ElHaiba.png',
+        imgAvailable: true,
+        linkedIn: 'https://www.linkedin.com/in/mariamelhaiba'
+      },
+      {
+        name: 'Amine Reguieg',
+        title: 'Project Manager',
+        img: 'team/Amine.png',
+        imgAvailable: true,
+        linkedIn: 'https://www.linkedin.com/in/amine-reguieg'
+      },
+      {
+        name: 'Samia Bouhaddou',
+        title: 'Data Engineer',
+        img: 'team/Samia.png',
+        imgAvailable: true,
+        linkedIn: 'https://www.linkedin.com/in/samia-bouhaddou-9b7331176'
+      },
+      {
+        name: 'Anastasia Zuikova',
+        title: 'Data Scientist',
+        img: 'team/Anastasiia.png',
+        imgAvailable: true,
+        linkedIn: 'https://www.linkedin.com/in/anastasiia-zuikova'
+      },
+      {
+        name: 'Sindhura Movva',
+        title: 'Full Stack Web Developer',
+        img: 'team/Sindhura.png',
+        imgAvailable: true,
+        linkedIn: 'https://www.linkedin.com/in/sindhura-movva-52012b2a8'
+      },
+      {
+        name: 'Elo Idiodi',
+        title: 'Full Stack Web Developer',
+        img: 'team/Elo.jpeg',
+        imgAvailable: true,
+        linkedIn: 'https://www.linkedin.com/in/elo-idiodi'
+      },
+      {
+        name: 'Anthony Misse',
+        title: 'Data Scientist',
+        img: 'team/Anthony.jpg',
+        imgAvailable: true,
+        linkedIn: 'https://www.linkedin.com/in/anthony-misse-3044a11b1'
+      },
+      {
+        name: 'Danyil Serhiienko',
+        title: 'UI/UX Designer',
+        img: 'team/Dan.png',
+        imgAvailable: true,
+        linkedIn: 'https://www.linkedin.com/in/danyil-serhiienko/'
+      },
+      {
+        name: 'Ana Ramirez',
+        title: 'Project Manager',
+        img: 'team/Ana.jpg',
+        imgAvailable: true,
+        linkedIn: 'https://www.linkedin.com/in/anaramirezl'
+      },
+      {
+        name: 'Mariam Jeda',
+        title: 'UI/UX Designer',
+        img: 'team/Mariam.png',
+        imgAvailable: true,
+        linkedIn: 'https://www.linkedin.com/in/mariam-o-sanni'
+      },
+      
+    ]
+  }
 
   onOverlayClick(event: MouseEvent): void {
     if (event.target === event.currentTarget) {
