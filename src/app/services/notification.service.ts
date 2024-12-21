b83e021ac0e7a5dec376a46a09770140d99fddb8
quispcchc <quisp@carlingtonchc.org>
2024-12-21 01:36:32 -0500

    include notification status

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -5,17 +5,24 @@ import { Injectable } from '@angular/core';
 })
 export class NotificationService {
   private message: string = '';
+  private status: 'success' | 'error' | null = null;
 
-  show(message: string): void {
+  show(message: string, status: 'success' | 'error' | null = 'success'): void {
     this.message = message;
+    this.status = status;
     setTimeout(() => this.clear(), 3000); // Clear message after 3 seconds
   }
 
   clear(): void {
     this.message = '';
+    this.status = null;
   }
 
   getMessage(): string {
     return this.message;
+  }
+
+  getStatus(): 'success' | 'error' | null {
+    return this.status;
   }
 }
